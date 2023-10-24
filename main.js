ce1e524f67b19a37d9ad4bb28c052bbb92d15909
asmaabarj <asmaabarj5@gmail.com>
2023-10-24 14:18:48 +0100

    mettre en place des verifications a l'aide de regex en js pour assurer que les informations saisies sont valides

--- main.js
+++ main.js
@@ -21,4 +21,46 @@ questionButtons.forEach(button => {
 });
 
 
+function validerFormulaire() {
+  var nom = document.getElementById("nom").value;
+  var prenom = document.getElementById("prenom").value;
+  var cin = document.getElementById("cin").value;
+  var email = document.getElementById("email").value;
+  var telephone = document.getElementById("telephone").value;
+
+  // Expressions régulières pour la validation
+  var regexNomPrenom = /^[A-Za-zÀ-ÖØ-öø-ÿ\s-]{2,}$/;
+  var regexCIN = /^[A-Za-z]{0,2}\d+$/;
+  var regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var regexTelephone = /^[0-9]{10}$/;
+
+  if (!regexNomPrenom.test(nom)) {
+    alert("Le nom n'est pas valide. Veuillez utiliser des lettres et des espaces.");
+    return ;
+  }
+
+  if (!regexNomPrenom.test(prenom)) {
+    alert("Le prénom n'est pas valide. Veuillez utiliser des lettres et des espaces.");
+    return ;
+  }
+
+  if (!regexCIN.test(cin)) {
+    alert("Le numéro de CIN n'est pas valide.");
+    return ;
+  }
+
+  if (!regexEmail.test(email)) {
+    alert("L'adresse e-mail n'est pas valide. Veuillez entrer une adresse e-mail valide.");
+    return ;
+  }
+
+  if (!regexTelephone.test(telephone)) {
+    alert("Le numéro de téléphone n'est pas valide. Il doit comporter 10 chiffres.");
+    return ;
+  }
+
+  // Si toutes les validations passent, le formulaire est soumis
+  alert("Inscription réussie !");
+  return ;
+}
 
